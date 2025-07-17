cdb8ab2ad0e6afb7505f793db572ec0c8934168d
wawan hermawan <wawanher487@gmail.com>
2025-07-17 08:15:28 +0700

    merubah struktur kuisoner

--- resources/views/components/form/personal-info.blade.php
+++ resources/views/components/form/personal-info.blade.php
@@ -1,4 +1,5 @@
-<div class="flex basis-full flex-col space-y-5 rounded-lg border border-gray-200 bg-white px-5 py-5 shadow dark:border-gray-700 dark:bg-gray-800">
+<div
+    class="flex basis-full flex-col space-y-5 rounded-lg border border-gray-200 bg-white px-5 py-5 shadow dark:border-gray-700 dark:bg-gray-800">
     <h5 class="mb-5 text-center text-2xl font-medium tracking-tight text-gray-900 dark:text-white">
         Data Diri
     </h5>
@@ -7,19 +8,24 @@
         <input type="hidden" name="question" value="1">
 
         <div class="mb-5">
-            <label for="name" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Nama Lengkap</label>
-            <input type="text" id="name" name="name" value="{{ old('name') }}" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+            <label for="name" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Nama
+                Lengkap</label>
+            <input type="text" id="name" name="name" value="{{ old('name') }}"
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
             @error('name')
                 <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
             @enderror
         </div>
 
         <div class="mb-5">
-            <label for="genders" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Jenis Kelamin</label>
-            <select id="genders" name="gender" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+            <label for="genders" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Jenis
+                Kelamin</label>
+            <select id="genders" name="gender"
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
                 <option value="" hidden>-Pilih-</option>
                 @foreach ($genders as $item)
-                    <option value="{{ $item->value }}" {{ old('gender') == $item->value ? 'selected' : '' }}>{{ $item->label }}</option>
+                    <option value="{{ $item->value }}" {{ old('gender') == $item->value ? 'selected' : '' }}>
+                        {{ $item->label }}</option>
                 @endforeach
             </select>
             @error('gender')
@@ -29,18 +35,22 @@
 
         <div class="mb-5">
             <label for="age" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Umur</label>
-            <input type="text" id="age" name="age" value="{{ old('age') }}" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+            <input type="text" id="age" name="age" value="{{ old('age') }}"
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
             @error('age')
                 <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
             @enderror
         </div>
 
         <div class="mb-5">
-            <label for="educations" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Pendidikan</label>
-            <select id="educations" name="education" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+            <label for="educations"
+                class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Pendidikan</label>
+            <select id="educations" name="education"
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
                 <option value="" hidden>-Pilih-</option>
                 @foreach ($educations as $item)
-                    <option value="{{ $item->value }}" {{ old('education') == $item->value ? 'selected' : '' }}>{{ $item->label }}</option>
+                    <option value="{{ $item->value }}" {{ old('education') == $item->value ? 'selected' : '' }}>
+                        {{ $item->label }}</option>
                 @endforeach
             </select>
             @error('education')
@@ -50,10 +60,12 @@
 
         <div class="mb-5">
             <label for="jobs" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Pekerjaan</label>
-            <select id="jobs" name="job" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+            <select id="jobs" name="job"
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
                 <option value="" hidden>-Pilih-</option>
                 @foreach ($jobs as $item)
-                    <option value="{{ $item->value }}" {{ old('job') == $item->value ? 'selected' : '' }}>{{ $item->label }}</option>
+                    <option value="{{ $item->value }}" {{ old('job') == $item->value ? 'selected' : '' }}>
+                        {{ $item->label }}</option>
                 @endforeach
             </select>
             @error('job')
@@ -62,24 +74,14 @@
         </div>
 
         <div class="mb-5">
-            <label for="villages" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Satuan Kerja</label>
-            <select id="villages" name="village" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
-                <option value="" hidden>-Pilih-</option>
-                @foreach ($villages as $item)
-                    <option value="{{ $item->id }}" {{ old('village') == $item->id ? 'selected' : '' }}>{{ $item->village }}</option>
-                @endforeach
-            </select>
-            @error('village')
-                <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div class="mb-5">
-            <label for="domiciles" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Tempat Tinggal</label>
-            <select id="domiciles" name="domicile" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+            <label for="domiciles" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Tempat
+                Tinggal</label>
+            <select id="domiciles" name="domicile"
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
                 <option value="" hidden>-Pilih-</option>
                 @foreach ($domiciles as $item)
-                    <option value="{{ $item->value }}" {{ old('domicile') == $item->value ? 'selected' : '' }}>{{ $item->label }}</option>
+                    <option value="{{ $item->value }}" {{ old('domicile') == $item->value ? 'selected' : '' }}>
+                        {{ $item->label }}</option>
                 @endforeach
             </select>
             @error('domicile')
@@ -87,7 +89,36 @@
             @enderror
         </div>
 
-        <div class="mb-5 text-right">
+        <div class="mb-5">
+            <label for="villages" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Satuan
+                Kerja</label>
+            <select id="villages" name="village"
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+                <option value="" hidden>-Pilih-</option>
+                @foreach ($villages as $item)
+                    <option value="{{ $item->id }}" {{ old('village') == $item->id ? 'selected' : '' }}>
+                        {{ $item->village }}</option>
+                @endforeach
+            </select>
+            @error('village')
+                <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+            @enderror
+        </div>
+        <!-- Jenis Pelayanan -->
+        <div>
+            <label for="jenis_pelayanan" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Jenis
+                Pelayanan</label>
+            <select id="jenis_pelayanan" name="jenis_pelayanan" required
+                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+                <option value="">- Pilih -</option>
+                <option value="pembuatan_ktp">Pembuatan KTP</option>
+                <option value="pembuatan_kk">Pembuatan KK</option>
+                <option value="legalisasi">Legalisasi</option>
+                <option value="konsultasi">Konsultasi</option>
+                <option value="lainnya">Lainnya</option>
+            </select>
+        </div>
+        <div class="mt-5 mb-5 text-right">
             <x-button.submit text="Selanjutnya" id="submit-personal-info" />
         </div>
     </form>
